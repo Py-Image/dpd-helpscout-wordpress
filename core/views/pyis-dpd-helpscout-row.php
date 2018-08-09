9efcb1483280f91063f1210659fe4eeb6adc1458
Eric Defore <eric.defore@gmail.com>
2018-08-09 09:56:35 -0400

    Message if no purchases found

--- core/views/pyis-dpd-helpscout-row.php
+++ core/views/pyis-dpd-helpscout-row.php
@@ -18,46 +18,58 @@ defined( 'ABSPATH' ) || die();
 <br /><br />
 
 <ul class="unstyled">
-	<?php foreach ( $purchases as $purchase ) : ?>
 	
-		<?php foreach ( $purchase->line_items as $download ) : ?>
+	<?php if ( $purchases ) : ?>
 	
-			<li>
-				
-				<div class="dpd-form">
-				
-					<?php echo $download->product_name; ?>
-					
-					<span class="button-container" style="display: none;">
-						
-						 - 
+		<?php foreach ( $purchases as $purchase ) : ?>
 
-						<a href="#" class="dpd-regenerate" title="<?php _e( 'Regenerate Access', 'pyis-dpd-helpscout' ); ?>">
-							<span class="badge green"><?php _e( 'Regenerate Access', 'pyis-dpd-helpscout' ); ?></span>
-						</a>
-						
-					</span>
+			<?php foreach ( $purchase->line_items as $download ) : ?>
 
-					<span class="hidden-input product_id" style="display: none;"><?php echo $download->product_id; ?></span>
-					<span class="hidden-input purchase_id" style="display: none;"><?php echo $download->purchase_id; ?></span>
-					<span class="hidden-input id" style="display: none;"><?php echo $download->id; ?></span>
-					<span class="hidden-input customer_id" style="display: none;"><?php echo $purchase->customer->id; ?></span>
+				<li>
 
-					<ul class="indent">
+					<div class="dpd-form">
 
-						<li><?php _e( 'Downloaded:', 'pyis-dpd-helpscout' ); ?> <?php echo $download->download_count; ?></li>
-						<li><?php _e( 'Can be downloaded:', 'pyis-dpd-helpscout' ); ?> <?php echo ( ! $download->download_limit ) ? __( 'Unlimited', 'pyis-dpd-helpscout' ) : $download->download_limit; ?></li>
-						<li><?php _e( 'Expires:', 'pyis-dpd-helpscout' ); ?> <?php echo get_date_from_gmt( date( 'Y-m-d H:i:s', $download->expires_at ), get_option( 'date_format', 'F j, Y' ) . ' @ ' . get_option( 'time_format', 'g:i a' ) . ' T' ); ?></li>
+						<?php echo $download->product_name; ?>
 
-					</ul>
-					
-				</div>
+						<span class="button-container" style="display: none;">
 
-			</li>
-	
+							 - 
+
+							<a href="#dpd-generate" class="dpd-regenerate" title="<?php _e( 'Regenerate Access', 'pyis-dpd-helpscout' ); ?>">
+								<span class="badge green"><?php _e( 'Regenerate Access', 'pyis-dpd-helpscout' ); ?></span>
+							</a>
+
+						</span>
+
+						<span class="hidden-input product_id" style="display: none;"><?php echo $download->product_id; ?></span>
+						<span class="hidden-input purchase_id" style="display: none;"><?php echo $download->purchase_id; ?></span>
+						<span class="hidden-input id" style="display: none;"><?php echo $download->id; ?></span>
+						<span class="hidden-input customer_id" style="display: none;"><?php echo $purchase->customer->id; ?></span>
+
+						<ul class="indent">
+
+							<li><?php _e( 'Downloaded:', 'pyis-dpd-helpscout' ); ?> <?php echo $download->download_count; ?></li>
+							<li><?php _e( 'Can be downloaded:', 'pyis-dpd-helpscout' ); ?> <?php echo ( ! $download->download_limit ) ? __( 'Unlimited', 'pyis-dpd-helpscout' ) : $download->download_limit; ?></li>
+							<li><?php _e( 'Expires:', 'pyis-dpd-helpscout' ); ?> <?php echo get_date_from_gmt( date( 'Y-m-d H:i:s', $download->expires_at ), get_option( 'date_format', 'F j, Y' ) . ' @ ' . get_option( 'time_format', 'g:i a' ) . ' T' ); ?></li>
+
+						</ul>
+
+					</div>
+
+				</li>
+
+			<?php endforeach; ?>
+
 		<?php endforeach; ?>
 	
-	<?php endforeach; ?>
+	<?php else : ?>
+	
+		<li>
+			<?php printf( __( 'No purchases found for %s', 'pyis-dpd-helpscout' ), $email ); ?>
+		</li>
+	
+	<?php endif; ?>
+	
 </ul>
 
 <div class="divider"></div>
