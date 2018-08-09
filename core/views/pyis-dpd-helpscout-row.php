3279f0178e5e8e3e2e081fdd0e2cedb5dc60cfb7
Eric Defore <eric.defore@gmail.com>
2018-08-08 22:37:04 -0400

    DOM to convert to Form via Chrome Extension

--- core/views/pyis-dpd-helpscout-row.php
+++ core/views/pyis-dpd-helpscout-row.php
@@ -24,24 +24,28 @@ defined( 'ABSPATH' ) || die();
 	
 			<li>
 				
-				<?php echo $download->product_name; ?> - 
+				<div class="dpd-form">
 				
-				<a href="#" class="dpd-regenerate" title="<?php _e( 'Regenerate Access', 'pyis-dpd-helpscout' ); ?>">
-					<span class="badge green"><?php _e( 'Regenerate Access', 'pyis-dpd-helpscout' ); ?></span>
-				</a>
-				
-				<span class="hidden-input product_id" style="display: none;"><?php echo $download->product_id; ?></span>
-				<span class="hidden-input purchase_id" style="display: none;"><?php echo $download->purchase_id; ?></span>
-				<span class="hidden-input id" style="display: none;"><?php echo $download->id; ?></span>
-				<span class="hidden-input customer-id" style="display: none;"><?php echo $purchase->customer->id; ?></span>
-				
-				<ul class="indent">
-						
-					<li><?php _e( 'Downloaded:', 'pyis-dpd-helpscout' ); ?> <?php echo $download->download_count; ?></li>
-					<li><?php _e( 'Can be downloaded:', 'pyis-dpd-helpscout' ); ?> <?php echo ( ! $download->download_limit ) ? __( 'Unlimited', 'pyis-dpd-helpscout' ) : $download->download_limit; ?></li>
-					<li><?php _e( 'Expires:', 'pyis-dpd-helpscout' ); ?> <?php echo get_date_from_gmt( date( 'Y-m-d H:i:s', $download->expires_at ), get_option( 'date_format', 'F j, Y' ) . ' @ ' . get_option( 'time_format', 'g:i a' ) . ' T' ); ?></li>
+					<?php echo $download->product_name; ?> - 
 
-				</ul>
+					<a href="#" class="dpd-regenerate" title="<?php _e( 'Regenerate Access', 'pyis-dpd-helpscout' ); ?>">
+						<span class="badge green"><?php _e( 'Regenerate Access', 'pyis-dpd-helpscout' ); ?></span>
+					</a>
+
+					<span class="hidden-input product_id" style="display: none;"><?php echo $download->product_id; ?></span>
+					<span class="hidden-input purchase_id" style="display: none;"><?php echo $download->purchase_id; ?></span>
+					<span class="hidden-input id" style="display: none;"><?php echo $download->id; ?></span>
+					<span class="hidden-input customer-id" style="display: none;"><?php echo $purchase->customer->id; ?></span>
+
+					<ul class="indent">
+
+						<li><?php _e( 'Downloaded:', 'pyis-dpd-helpscout' ); ?> <?php echo $download->download_count; ?></li>
+						<li><?php _e( 'Can be downloaded:', 'pyis-dpd-helpscout' ); ?> <?php echo ( ! $download->download_limit ) ? __( 'Unlimited', 'pyis-dpd-helpscout' ) : $download->download_limit; ?></li>
+						<li><?php _e( 'Expires:', 'pyis-dpd-helpscout' ); ?> <?php echo get_date_from_gmt( date( 'Y-m-d H:i:s', $download->expires_at ), get_option( 'date_format', 'F j, Y' ) . ' @ ' . get_option( 'time_format', 'g:i a' ) . ' T' ); ?></li>
+
+					</ul>
+					
+				</div>
 
 			</li>
 	
